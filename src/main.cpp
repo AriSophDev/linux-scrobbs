db0744a22aae70650c93c260b05e179ed16ea542
iana Sophia <sophialarge57@gmail.com>
2026-01-15 16:22:49 -0500

    feat: Add playerctl data output and fix syntax error
    - Incorporated changes from the previous commit.
    - Resolved a syntax error by removing a duplicate '#include <cstudio>' directive.
    - Implemented playerctl data output within the 'main' function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <cstdio>
 #include <iostream>
-#include <cstdio>
 #include <sstream>
 #include <ostream>
 #include <string>
@@ -39,14 +38,37 @@ struct metadata {
     std::string title;
     std::string artist;
     std::string album;
+    std::string length;
 };
+
+
+metadata parse_metadata(const std::string& info) {
+    std::vector<std::string> lines = split_lines(info);
+    metadata data;
+
+    for (const auto& line : lines) {
+        if (line.find("xesam:title") != std::string::npos) {
+            data.title = line.substr(line.find(":") + 2);
+        } else if (line.find("xesam:artist") != std::string::npos) {
+            data.artist = line.substr(line.find(":") + 2);
+        } else if (line.find("xesam:album") != std::string::npos) {
+            data.album = line.substr(line.find(":") + 2);
+        } else if (line.find("mpris:length") != std::string::npos) {
+            data.length = line.substr(line.find(":") + 2);
+        }
+    }
+
+    return data;
+}
 
 
 
 int main() {
     std::string info = run_command("playerctl metadata");
-
-    std::cout << "salida de playerctl: " << std::endl;
-    std::cout << info << std::endl;
+    metadata data = parse_metadata(info);
+    std::cout << "Title: " << data.title << std::endl;
+    std::cout << "Artist: " << data.artist << std::endl;
+    std::cout << "Album: " << data.album << std::endl;
+    std::cout << "Length (ns) " << data.length << std::endl;
     return 0;
 }
