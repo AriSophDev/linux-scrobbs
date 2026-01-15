f1b19c7d6fd1fa3a80cac932174af97bb7ad48f5
Sophia Large <sophialarge57@gmail.com>
2026-01-14 23:06:25 -0500

    Main is now functional: it reads and processes playerctl output

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
+#include <cstdio>
 #include <iostream>
-#include <cstudio>
+#include <cstdio>
+#include <ostream>
 #include <string>
 
-std::string run_command(const char* cmd)
+std::string run_command(const char* cmd){
+    char buffer [256];
+    std::string result;
 
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) return "";
 
+    while (fgets(buffer, sizeof(buffer), pipe)) {
+        result += buffer;
+    }
+
+    pclose(pipe);
+    return result;
+}
+
+int main() {
+    std::string info = run_command("playerctl metadata");
+
+    std::cout << "salida de playerctl: " << std::endl;
+    std::cout << info << std::endl;
+    return 0;
+}
